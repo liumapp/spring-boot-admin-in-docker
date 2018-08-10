f168ea99b39bded378d5f8db866730883c013ee4
liumapp <liumapp.com@gmail.com>
2018-08-10 15:12:20 +0800

    tmp

---
+++ admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
@@ -0,0 +1,14 @@
+package com.liumapp.demo.admin.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file AdminServerMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/10
+ */
+@SpringBootApplication
+public class AdminServerMain {
+}
