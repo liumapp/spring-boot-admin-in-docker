c85b0dfe32abed909ac1f28490779ffded966ee0
liumapp <liumapp.com@gmail.com>
2018-08-10 10:31:13 +0800

    add admin client main class

---
+++ admin-client/src/main/java/com/liumapp/demo/admin/client/AdminClientMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file AdminClientMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/10
+ */
+@SpringBootApplication
+public class AdminClientMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(AdminClientMain.class, args);
+    }
+
+}
