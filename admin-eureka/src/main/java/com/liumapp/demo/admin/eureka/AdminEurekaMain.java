0600f5c659d8b210118f5469399d289c6b9b9c3d
liumapp <liumapp.com@gmail.com>
2018-08-10 09:25:19 +0800

    add main clas s

---
+++ admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
@@ -0,0 +1,22 @@
+package com.liumapp.demo.admin.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liumapp
+ * @file AdminEurekaMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class AdminEurekaMain {
+
+    public static void main (String[] args) {
+        SpringApplication.run(AdminEurekaMain.class, args);
+    }
+
+}
--- admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.liumapp.demo.admin.eureka;
