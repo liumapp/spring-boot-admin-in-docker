6d5ed056f929eefb42297864f005095b2a61079d
liumapp <liumapp.com@gmail.com>
2018-09-11 09:43:22 +0800

    add spring securiry

--- admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
+++ admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
@@ -3,7 +3,10 @@ package com.liumapp.demo.admin.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author liumapp
@@ -14,9 +17,21 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class AdminServerMain {
+
     public static void main(String[] args) {
         SpringApplication.run(AdminServerMain.class, args);
     }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+
 }
