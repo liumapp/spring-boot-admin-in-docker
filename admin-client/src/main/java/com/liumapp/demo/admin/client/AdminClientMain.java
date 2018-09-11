0d90485cb20f93c54e12664880579b8db87f0646
liumapp <liumapp.com@gmail.com>
2018-09-11 14:57:57 +0800

    register client into eureka success

--- admin-client/src/main/java/com/liumapp/demo/admin/client/AdminClientMain.java
+++ admin-client/src/main/java/com/liumapp/demo/admin/client/AdminClientMain.java
@@ -2,6 +2,7 @@ package com.liumapp.demo.admin.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liumapp
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2018/8/10
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminClientMain {
 
     public static void main (String[] args) {
--- admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
+++ admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
@@ -1,5 +1,6 @@
 package com.liumapp.demo.admin.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
