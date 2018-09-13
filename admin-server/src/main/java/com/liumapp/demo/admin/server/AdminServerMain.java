5f95e93ea239431cbfbbbbb39b9f55989bdcc7bb
liumapp <liumapp.com@gmail.com>
2018-09-13 16:17:43 +0800

    update admin-server dependencies

--- admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
+++ admin-server/src/main/java/com/liumapp/demo/admin/server/AdminServerMain.java
@@ -1,6 +1,5 @@
 package com.liumapp.demo.admin.server;
 
-import de.codecentric.boot.admin.config.AdminServerProperties;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,8 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author liumapp
