7dcf9755ef17938c94be8e50a58b4fdea11d4daf
liumapp <liumapp.com@gmail.com>
2018-09-14 20:23:40 +0800

    v2.2.6

--- rm-image.sh
+++ rm-image.sh
@@ -12,11 +12,11 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
-docker rmi liumapp/admin-client:v2.2.0
+docker rmi liumapp/admin-client:v2.2.6
 
-docker rmi liumapp/admin-server:v2.2.0
+docker rmi liumapp/admin-server:v2.2.6
 
-docker rmi liumapp/admin-eureka:v2.2.0
+docker rmi liumapp/admin-eureka:v2.2.6
 
 
 
