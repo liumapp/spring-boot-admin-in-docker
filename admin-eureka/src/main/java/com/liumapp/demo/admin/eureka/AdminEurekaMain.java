00f3525a7dfde88a3a5749f6fc8dbc55e4371859
liumapp <liumapp.com@gmail.com>
2018-09-17 08:50:35 +0800

    high advanced eureka is not used spring.security.basic.enable , must config in spring security itself

--- admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
+++ admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
@@ -1,9 +1,18 @@
 package com.liumapp.demo.admin.eureka;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author liumapp
@@ -18,6 +27,36 @@ public class AdminEurekaMain {
 
     public static void main (String[] args) {
         SpringApplication.run(AdminEurekaMain.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Value("${spring.security.name}")
+        private String username;
+
+        @Value("${spring.security.password}")
+        private String password;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+            http.csrf().disable();
+            //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        }
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                    //admin
+                    .withUser(username).password(password).roles("EUREKA-CLIENT").and()
+                    //eureka-security-client
+                    .withUser("eureka-security-client").password("eureka-security-client").roles("EUREKA-CLIENT")
+            ;
+        }
     }
 
 }
