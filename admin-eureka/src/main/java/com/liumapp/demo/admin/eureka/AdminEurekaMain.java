7c143d74998baa0084ce3d39c8ea4f90d3658e0f
liumapp <liumapp.com@gmail.com>
2018-09-17 09:05:56 +0800

    failed again

--- admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
+++ admin-eureka/src/main/java/com/liumapp/demo/admin/eureka/AdminEurekaMain.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 /**
  * @author liumapp
